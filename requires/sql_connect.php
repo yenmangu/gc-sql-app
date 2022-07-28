ff3eabdd68b48569a6d46ee47b4a38f53338bff1
yenmangu <robert.shelford@googlemail.com>
2022-07-28 22:14:25 +0100

    connection and initial db page written

---
+++ requires/sql_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+	$host = "localhost";
+	$dbName = "GadgetSolutions";
+	$username = "root";
+	$password = "4Vl6WDSVD4xr";
+	$dsn = "mysql:host=$host;dbname=$dbName";
+      //specify if local or production
+      //$liveUrl = "http://.....$device[slug]";
+      //$newUrl = "http://18.168.90.222$device[slug]";
+      //$localUrl="http://localhost$device[slug]";
+	
+?>
